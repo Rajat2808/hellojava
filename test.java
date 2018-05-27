ca994e370574cfba4d1f3ba99f807614af63a9fe
Rajat2808 <rb9728@gmail.com>
2018-05-27 13:47:12 +0530

    adding basic java program to remote repo

---
+++ test.java
@@ -0,0 +1,24 @@
+
+
+class test{
+
+public static void main(String[] args)
+{
+//Integer[] ar=new Integer[]{new Integer(5), new Integer(1),new Integer(2),new Integer(9), new Integer(3),new Integer(10)};
+//int[] ar={5,1,6,2,3,7};
+
+/*Integer[] ar=new Integer[]{5,1,2,9,3,10};
+Arrays.sort(ar,Collections.reverseOrder());
+
+for(int i:ar)
+System.out.println(ar[i]);*/
+
+/*String s1="what itsright another itsagain";
+System.out.println(s1.replaceFirst("its","beauty"));
+System.out.println(s1.replaceFirst("its(.*)","beauty"));
+System.out.println(s1);*/
+
+for(int i=0;i<10;i++)
+System.out.println("Hello world...."+i);
+}
+}
